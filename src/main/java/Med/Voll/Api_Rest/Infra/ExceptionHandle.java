b0618920afad7b37cdfef821a0c43ec622f7e9ab
VitorKreis <vitor.12richter@gmail.com>
2024-08-06 10:26:06 -0300

    add:slog:Melhorado a forma com que os erros aperecem, e que sao logados

---
+++ src/main/java/Med/Voll/Api_Rest/Infra/ExceptionHandle.java
@@ -0,0 +1,33 @@
+package Med.Voll.Api_Rest.Infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandle {
+
+
+    public record ResponseErrorArgumentNotValid(String campo, String message){
+        public ResponseErrorArgumentNotValid(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity ErrorNotFound(){
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity ErrorArgumentNotValid(MethodArgumentNotValidException exception){
+
+        var error = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(error.stream().map(ResponseErrorArgumentNotValid::new).toList());
+    }
+}
