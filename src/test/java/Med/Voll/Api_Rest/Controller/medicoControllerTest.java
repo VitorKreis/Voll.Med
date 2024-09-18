96708bf596c6a912941f937b2dcf87a72439ab7a
VitorKreis <vitor.12richter@gmail.com>
2024-09-18 10:37:55 -0300

    test: finalizado com os testes da rota de medicos

--- src/test/java/Med/Voll/Api_Rest/Controller/medicoControllerTest.java
+++ src/test/java/Med/Voll/Api_Rest/Controller/medicoControllerTest.java
@@ -2,7 +2,6 @@ package Med.Voll.Api_Rest.Controller;
 
 import Med.Voll.Api_Rest.domain.Endereco.DadosAtualizacaoEndereco;
 import Med.Voll.Api_Rest.domain.Endereco.DadosEndereco;
-import Med.Voll.Api_Rest.domain.Endereco.Endereco;
 import Med.Voll.Api_Rest.domain.Medico.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,14 +18,16 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 
 @SpringBootTest
@@ -49,32 +50,26 @@ class medicoControllerTest {
     JacksonTester<DadosMedico> dadosMedico;
 
 
+    @Autowired
+    JacksonTester<DadosAtualizacaoMedico> dadosAtualizaMedico;
+
+
     @Test
     @DisplayName("Dever retornar codigo 200")
-    void consultarMedicoCenarioTudoCerto() throws Exception {
-        var response = mvc.perform(get("/medico")).andReturn().getResponse();
+    void consultarMedicoCenarioPeloId() throws Exception {
+
+        when(repository.getReferenceById(ArgumentMatchers.anyLong())).thenReturn(new Medico());
+
+        var response = mvc.perform(get("/medico/20")).andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
-    @DisplayName("Dever retornar codigo 403, por id nao encontrado")
-    void consultarMedicoCenarioIDInexistente() throws Exception {
-        var response = mvc.perform(get("/medico/20")).andReturn().getResponse();
-
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
-    }
-
-
-
-    @Test
     @DisplayName("Deve retonar codigo 201, medico criado com sucesso")
-    void criarMedicoCenarioMedicoCriado() throws Exception {
-        var especialidade = Especialidade.NEUROLOGIA;
+    void CadadastrarMedicoCenarioMedicoCriado() throws Exception {
 
-        var endereco = new DadosEndereco("rua xpto", "bairro", "00000000", "12345678", "DF", null);
-
-        var dadoMedico = new DadosMedico("Vitor", "vitor.med@vollmed.com", "4199999999", "12334", especialidade, Tipo.CLINICO_GERAL, endereco);
+        var dadoMedico = medico();
 
         when(repository.save(ArgumentMatchers.any())).thenReturn(new Medico(dadoMedico));
 
@@ -85,5 +80,85 @@ class medicoControllerTest {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
     }
 
+    @Test
+    @DisplayName("Dever retornar codigo 400, por informaçoes invalidas")
+    void CadadastrarMedicoCenarioInformacoesInvalidas() throws Exception {
 
+        var response = mvc.perform(post("/medico")).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+
+    @Test
+    @DisplayName("Dever retornar codigo 403, por falta de credenciais")
+    @WithMockUser(authorities = "USER")
+    void CadadastrarMedicoCenarioAcessoInvalido() throws Exception {
+
+        var response = mvc.perform(post("/medico")).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+    @Test
+    @DisplayName("Deve retornar codigo 200, por inforamaçoes do medico atualizadas com sucesso")
+    void AtualizarMedicoCenarioMedicoAtualizado() throws Exception {
+
+        var dadoMedico = medico();
+
+        var dadosAtualizados = new DadosAtualizacaoMedico("Medico", dadoMedico.email(), dadoMedico.telefone(), new DadosAtualizacaoEndereco("rua xpto", "bairro", "00000000", "12345678", "DF"));
+
+        when(repository.getReferenceById(anyLong())).thenReturn(new Medico(dadoMedico));
+
+        var response = mvc.perform(put("/medico/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(dadosAtualizaMedico.write(dadosAtualizados).getJson())).andReturn().getResponse();
+
+
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+    }
+
+    @Test
+    @DisplayName("Dever retornar codigo 400, por informaçoes invalidas")
+    void AtualiozarMedicoCenarioInformacoesInvalidas() throws Exception {
+
+        var response = mvc.perform(put("/medico/20")).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Dever retornar codigo 403, por falta de credenciais")
+    @WithMockUser(authorities = "USER")
+    void AtualizarMedicoCenarioAcessoInvalido() throws Exception {
+
+        var response = mvc.perform(put("/medico")).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+    @Test
+    @DisplayName("Dever retornar codigo 200")
+    void DeleteMedicoCenarioMedicoDeletado() throws Exception {
+
+        when(repository.getReferenceById(anyLong())).thenReturn(new Medico(medico()));
+
+        var response = mvc.perform(delete("/medico/1")).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+
+
+
+
+    private DadosMedico medico(){
+        return new DadosMedico("Vitor", "vitor.med@vollmed.com", "4199999999", "12334", Especialidade.NEUROLOGIA, Tipo.CLINICO_GERAL, endereco());
+    }
+
+    private DadosEndereco endereco() {
+        return new DadosEndereco("rua xpto", "bairro", "00000000", "12345678", "DF", null);
+    }
 }
